40f2f2218580cb3c7e7966200ee688505bb2036e
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-08-27 21:36:25 +0000

    update js

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ function hideAnswer() {
 
 function populateQuestion(question,answer) {
   questionText.textContent = question;
-  answerText.textContent = "The answer is " + answer + ".";
+  answerText.textContent = "The answer is " + answer + "!";
 }
 
-populateQuestion("what is 2 + 2?", "4");
+populateQuestion("What is 2 + 2?", "4");
 
 showBtn.addEventListener('click', showAnswer);
 hideBtn.addEventListener('click', hideAnswer);
